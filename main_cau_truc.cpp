e64efa6957ca5ed625aa7fdf1f246455e3681b44
ginn1111 <67890264+ginn1111@users.noreply.github.com>
2021-06-03 13:03:45 +0700

    Add files via upload

--- main_cau_truc.cpp
+++ main_cau_truc.cpp
@@ -1,22 +1,132 @@
 #include <iostream>
 #include <cstring>
 #include <stdlib.h>
+#include <stdio.h>
 #include "xu_ly_may_bay.h"
 #include "xu_ly_chuyen_bay.h"
 #include "dat_ve.h"
 #include "io.h"
-
-/*
-Progblem
-
- Neu 2 chuyen bay co thoi gian khoi hanh khac nhau
- Mot hanh khach deu dat ve tren 2 chuyen bay do
- Thi cap nhat 1 trong 2 chuyen bay, chuyen bay nay khong the cung gio
- khoi hanh voi chuyen bay kia 
-
-*/
+#include "hash_table.h"
 
 using namespace std;
+
+void int2str(int s, char* res) {
+  itoa(s,res,10);
+  res = strrev(res);	
+  while(strlen(res) < 3)
+    strcat(res, "0");
+  res = strrev(res);
+}
+
+void giaLapMayBay(DanhSachMayBay &dsMB) {
+	char str[255];
+	char tmp[255];
+	int soCho = 5;
+	char strErr[255];
+	for(int i = 1; i <= 300; i++) {
+	    strcpy(str, "MB");
+	    int2str(i, tmp);
+	    strcat(str, tmp);
+	    themMayBayHopLe(newMayBay(str, "MAY BAY DAN DUNG", soCho), dsMB, strErr);
+	    cout << str << " " << strErr << endl;
+	}
+}
+
+void giaLapChuyenBay(DanhSachChuyenBay &dsCB, DanhSachMayBay &dsMB, DanhSachTam &dsTmp) {
+	char str[255];
+	char str2[255];
+	char tmp[255];
+	char strErr[255];
+	srand(time(NULL));
+	int ngaytmp[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	char *sanBayDen[] = {
+		"DA NANG",
+		"TAN SON NHAT",
+		"NOI BAI",
+		"CAM RANH",
+		"PHU QUOC"
+	};
+	int ranSanBay;
+	int ranSoHieu;
+	int ngay, thang, nam, gio, phut;
+	NgayThangNam dt;
+	for(int i = 1; i <= 600; i++) { 
+		// random thoi gian khoi hanh
+		nam = 2021;
+		thang = rand() % (12 - 6 + 1) + 6;
+		ngay = rand() % (ngaytmp[thang] - 4 + 1) + 4;
+		gio = rand() % (24 - 1 + 1) + 1;
+		phut = rand() & 59;
+		// random san bay den
+		ranSanBay = rand() % 5;
+		// random soHieu
+		
+		strcpy(str, "MB");
+		strcpy(str2, "CB");
+		
+		ranSoHieu = rand() % (300 - 1 + 1) + 1;
+	    int2str(ranSoHieu, tmp);
+	    strcat(str, tmp);
+	    
+	    int2str(i, tmp);
+	    strcat(str2, tmp);
+	    
+	    dt = newNgayThangNam(ngay, thang, nam, gio, phut);
+	    
+	    themChuyenBayHopLe(
+			newNodeChuyenBay(
+				newChuyenBay(str2,dt,sanBayDen[ranSanBay],str)
+			), dsCB, dsMB, dsTmp, strErr);
+			
+		cout << str2 << " " << strErr << endl;
+		dt.toString();
+		cout << endl;
+	}
+}
+void randomCMND(char *CMND) {
+	char buff[2];
+	int randNum;
+	strcpy(CMND, "");
+	srand(time(NULL));
+	for(int i = 1; i <= 12; i++) {
+		randNum = rand() % 9;
+		itoa(randNum,buff,10);
+		strcat(CMND, buff);
+	}
+	cout << CMND << endl;
+}
+
+void giaLapDSHK(DanhSachHanhKhach &dsHK) {
+	char CMND[MAXCMND];
+	int phai;
+	char *ho[] = {
+		"NGUYEN VAN",
+		"PHAM VAN",
+		"LE THI",
+		"NGUYEN THI",
+		"HUYNH VAN",
+		"CAO THI",
+	};
+	char *ten[] = {
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F"
+	};
+	
+	int iTen, iHo;
+	srand(time(NULL));
+	for(int i = 1; i <= 20; i++) {
+		randomCMND(CMND);
+		phai = rand() % 2;
+		iHo = rand() % 6;
+		iTen = rand() % 6;
+		themHanhKhach(dsHK, newHanhKhach(CMND, ho[iHo], ten[iTen], phai));
+		system("pause");
+	}
+}
 
 int main() {
 	
@@ -24,71 +134,39 @@ int main() {
 	DanhSachChuyenBay dsCB = NULL;
 	DanhSachHanhKhach dsHK = NULL;
 	DanhSachTam dsTmp;
-	NgayThangNam dt = newNgayThangNam(12,6,2021,3,3);
-	
+	HashTable table;
+
 	layDSMayBay(dsMB);
 	layDSChuyenBay(dsCB);
 	layDSHK(dsHK);
-
-//	 themMayBayHopLe(newMayBay("MB001", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB002", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB003", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB004", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB005", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB006", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB007", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB008", "MAY BAY DAN DUNG", 3), dsMB);
-//	 themMayBayHopLe(newMayBay("MB009", "MAY BAY DAN DUNG", 3), dsMB);
-
-//
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB001", newNgayThangNam(1,6,2021, 3,3), "DA NANG", "MB001")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB002", newNgayThangNam(21,6,2021, 3,3), "TP HCM", "MB002")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB003", newNgayThangNam(19,7,2021, 3,3), "DA NANG", "MB003")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB004", newNgayThangNam(22,6,2021, 3,3), "DA NANG", "MB004")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB005", newNgayThangNam(10,7,2021, 3,3), "TP HCM", "MB005")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB006", newNgayThangNam(5,6,2021, 3,3), "DA NANG", "MB006")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB007", newNgayThangNam(30,7,2021, 3,3), "TAN SON NHAT", "MB007")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB008", newNgayThangNam(27,6,2021, 3,3), "DA NANG", "MB008")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB009", newNgayThangNam(14,7,2021, 3,3), "TAN SON NHAT", "MB001")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB010", newNgayThangNam(12,6,2021, 3,3), "DA NANG", "MB002")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB011", newNgayThangNam(15,7,2021, 3,3), "DA NANG", "MB003")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB012", newNgayThangNam(3,6,2021, 3,3), "TP HCM", "MB004")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB013", newNgayThangNam(4,7,2021, 3,3), "VUNG TAU", "MB005")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB014", newNgayThangNam(6,6,2021, 3,3), "TAN SON NHAT", "MB006")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB015", newNgayThangNam(31,7,2021, 3,3), "NOI BAI", "MB007")), dsCB, dsMB);
-//	 themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB017", newNgayThangNam(25,6,2021, 18,0), "TP HCM", "MB008")), dsCB, dsMB);
-	 dsTmp = dsNode2DsTmpConVe(dsCB);
-
-//	datVe("123456789", 1, dsTmp.cb[0],dsHK, dsCB);
-//	datVe("1234567891", 2, dsTmp.cb[0],dsHK, dsCB);
-//	datVe("123456789", 1, dsTmp.cb[1],dsHK, dsCB);
-//	char strErr[255];
-//	if(!capNhatNgayThangNam(dsCB, dsTmp.cb[1],dt,strErr)) {
-//		cout << strErr << endl;
-//	}
-//	xuatDSHKDuaVaoMaCB(dsCB, dsHK, "CB002");
-//	capNhatDanhSachVe(dsTmp.cb[0], "126", 1);
-//	themChuyenBayHopLe(newNodeChuyenBay(newChuyenBay("CB003", newNgayThangNam(23,4,2021, 18,54), "DA NANG", "MB001")), dsCB, dsMB);
-//	datVe("128", dsHK, 1, dsTmp, 2, dsCB);
 	
+	dsTmp = dsNode2DsTmp(dsCB);
+//	cout << dsTmp.n << endl;
+	
+//	giaLapMayBay(dsMB);
+//	giaLapChuyenBay(dsCB, dsMB, dsTmp);
+//	giaLapDSHK(dsHK);
+
 //	xuatDSMB(dsMB);
-//	 xuatDSCB(dsCB, dsMB);
+//	xuatDSCB(dsCB, dsMB);
+//	xuatDSHK(dsHK);
 	xuatDSTmp(dsTmp);
-	xuatDSHK(dsHK);
-//	xuatDSHKDuaVaoMaCB(dsCB, dsHK, "CB001");
-	// xuatDSCBConVeKhoiHanhTrongNgayDenNoi("DA NANG", dt, dsCB);
-	ListHanhKhachThongKe list;
-	xuatDSHKDuaVaoMaCB(dsCB, dsHK, "CB001", list);
-	xuatListHKTK(list);
+
+//	initHashTable(table);
+//	dsMBToHashTable(dsMB, table);
+//	printHashTable(table);
+//	thongKeMBThucHienCB(table, dsCB);
 	
 	luuDSMayBay(dsMB);
 	luuDSChuyenBay(dsCB);
 	luuDSHK(dsHK);
+
 	
 	clearDSHK(dsHK);
 	clearDSCB(dsCB);
 	clearDSMB(dsMB);
 	clearDSTmp(dsTmp);
+
 	
 	return 0;
 }
