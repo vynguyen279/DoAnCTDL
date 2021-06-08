7bb6a8c1e6fc786984d8ac8730657107ec8c1290
vynguyen279 <80610537+vynguyen279@users.noreply.github.com>
2021-06-08 19:58:49 +0700

    Update pageQLV.h

--- pageQLV.h
+++ pageQLV.h
@@ -1,8 +1,7 @@
-  
+
 #pragma once
 #include "reuse.h"
-#include<math.h>
-#include<string>
+#include <math.h>
 #define GRAY COLOR(156, 156, 156)
 
 
@@ -36,7 +35,7 @@
 void drawPageQLV_DSCB(short **mapID,Shape *shape);
 void drawPageQLV_DSHK(short **mapID,Shape *shape);
 void drawPageQLV_DatVe(short **mapID,Shape *shape);
-
+//===========================XU LY=============================================
 void colorNotWorkingSeat(Button *button,Shape *shape,short **mapID){
 	int  inputSeat = 59; //dung ham get input
 	int So_ghe_khong_hd = 72-inputSeat; 
@@ -62,7 +61,17 @@ void colorNotWorkingSeat(Button *button,Shape *shape,short **mapID){
 		j=0;
 	}
 }
-//==========================Page QLV=======================================
+DanhSachTam dsCBConVe( DanhSachChuyenBay &dsCB) {
+	DanhSachTam dsTmp;
+	initDanhSachTam(dsTmp);
+	NodeChuyenBay *nodeChay = dsCB;
+	for(; nodeChay != NULL; nodeChay = nodeChay->next) {
+		capNhatTrangThaiHoanTat(nodeChay->chuyenBay);
+		if(nodeChay->chuyenBay.trangThai==1)
+			themPhanTuVaoDSTmp(&(nodeChay->chuyenBay), dsTmp);
+	}
+	return dsTmp;
+}
 void resetNumber(){
 	setfillstyle(1, 15);
     bar(450, 150, 450+480, 150+20);
@@ -77,9 +86,9 @@ void PrintPresentPage(short presentPage){
 	short end = start+11;
 	short count = 1;
 	setText(2,10,0,15);
+	char pchar[5];
    for(int i=start;i<=end;i++){
-   	 	std::string s = std::to_string(i);
-     char const *pchar = s.c_str();
+   	 	itoa(i,pchar,10);
      if(i>=10)
      outtextxy(455+40*(count-1), 150, pchar);
      else
@@ -89,9 +98,8 @@ void PrintPresentPage(short presentPage){
 }
 void drawSeatMap(short **mapID, Shape *shape, short presentPage){
 	Button button_seat[360];
-    for(int i = 0;i<360;i++){
+    for(int i = 0;i<360;i++)
       	button_seat[i]={0,0,0,0,0,0};
-	}
 
 	short x,y,ID,temp,vtr, tempVtr;
 	for(int i = 0;i <12;i++){
@@ -113,6 +121,79 @@ void drawSeatMap(short **mapID, Shape *shape, short presentPage){
         }
     }
 }
+void updateNumOfBoardDSCB_conve(int &numOfPage, int presentPage,DanhSachTam &dsTmp){
+	numOfPage = (dsTmp.n - 1) / 10 + 1;
+    char toChar[5];
+    setText();
+    setfillstyle(1, 15);
+    bar(676, 90, 715, 115);
+    itoa(presentPage, toChar, 10);
+    outtextxy(675, 95, toChar);
+    outtextxy(675 + 15, 95, "/");
+    itoa(numOfPage, toChar, 10);
+    outtextxy(675 + 30, 95, toChar);
+}
+void outCB_conve(Board &board,DanhSachTam &dsTmp, short ID){
+	setText();
+    short x1 = board.x1,
+	y1 = (ID%10 + 1) * board.heightOfLine + board.y1, 
+	x2 = x1;
+    for(int col = 1; col<=board.numOfCol; col++){
+    	x1 = x2;
+    	x2 += board.widthOfCol[col - 1];   
+      
+        if (col == 1) //MA CB
+        { 
+            outtextxy(x1 + board.widthOfCol[col - 1] / 2 - 4.5 * strlen(dsTmp.cb[ID]->maChuyenBay),
+                      y1 + (board.heightOfLine) / 2 - 10, dsTmp.cb[ID]->maChuyenBay);
+        }
+        else if (col == 2) //SO HIEU
+            outtextxy(x1 + board.widthOfCol[col - 1] / 2 - 4.5 * strlen(dsTmp.cb[ID]->soHieuMayBay),
+                      y1 + (board.heightOfLine) / 2 - 10, 	dsTmp.cb[ID]->soHieuMayBay);
+        else if (col == 3)  //NGAY GIO KHOI HANH
+        { 		
+			char phut[5], gio[5],ngay[5],thang[5],nam[5],ngayGio[25];
+			itoa(dsTmp.cb[ID]->ngayKhoiHanh.phut, phut, 10);
+			itoa(dsTmp.cb[ID]->ngayKhoiHanh.gio, gio, 10);
+			itoa(dsTmp.cb[ID]->ngayKhoiHanh.ngay, ngay, 10);
+			itoa(dsTmp.cb[ID]->ngayKhoiHanh.thang, thang, 10);
+			itoa(dsTmp.cb[ID]->ngayKhoiHanh.nam, nam, 10);
+			if(strlen(ngay)==1)  {
+				strcpy(ngayGio,"0");
+				strcat(ngayGio,ngay);
+			}
+			else	strcpy(ngayGio,ngay);
+			strcat(ngayGio,"/");
+			if(strlen(thang) == 1) strcat(ngayGio,"0");
+			strcat(ngayGio,thang);
+			strcat(ngayGio,"/");
+			strcat(ngayGio,nam);
+			strcat(ngayGio," - ");
+			if(strlen(gio) == 1) strcat(ngayGio,"0");
+			strcat(ngayGio,gio);
+			strcat(ngayGio,":");
+			if(strlen(phut) == 1) strcat(ngayGio,"0");
+			strcat(ngayGio,phut);
+            outtextxy(x1 + board.widthOfCol[col - 1] / 2 - 3 * strlen(ngayGio),
+                      y1 + (board.heightOfLine) / 2 - 10, ngayGio);
+        }
+        else if (col == 4) //SAN BAY DEN
+        { 
+
+            outtextxy(x1 + board.widthOfCol[col - 1] / 2 - 4.5 * strlen(dsTmp.cb[ID]->sanBayDen),
+                      y1 + (board.heightOfLine) / 2 - 10, 	dsTmp.cb[ID]->sanBayDen);
+        }
+        else if (col == 5) //TRANG THAI
+        { 	
+            char trangThai[15];
+		    if(dsTmp.cb[ID]->trangThai == 1)	strcpy(trangThai,"CON VE");
+            outtextxy(x1 + board.widthOfCol[col - 1] / 2 - 4 * strlen(trangThai),
+                      y1 + (board.heightOfLine) / 2 - 10, trangThai);						
+        }        	
+    }
+} 
+
+//==========================Page QLV=======================================
 
 void checkHoverPageQLV_DSHK(short &newID, short lastID, Shape *shape, short **mapID){
       if (ismouseclick(WM_MOUSEMOVE))
@@ -225,6 +306,7 @@ void checkHoverPageQLV_DatVe(short &newID, short lastID, Shape *shape, short **m
 }
 
 
+
 void checkEventPageQLV_DSHK(Shape *shape, short **mapID){
     int x, y;
     short ID = -1, lastID = -1;
@@ -275,22 +357,26 @@ void checkEventPageQLV_DatVe(Shape *shape, short **mapID){
 						return;
 					}
 					case ID_SEAT_MAP_PRE_BUTTON:{
-						if(presentPage>1){
+						if(presentPage > 1){
 							presentPage--;
 							resetNumber();
 							PrintPresentPage(presentPage);
-
+//                            resetSeatMap(mapID);
+//                            drawSeatMap(mapID,shape,presentPage);
 						}
 						break;
 					}
 					case ID_SEAT_MAP_NEXT_BUTTON:{
-						if(presentPage<totalPage){
+						if(presentPage < totalPage){
 							presentPage++;
 							resetNumber();
 							PrintPresentPage(presentPage);
+//							resetSeatMap(mapID);
+//                            drawSeatMap(mapID,shape,presentPage);
 						}
 						break;
 					}
+					
 					default:
 						break;
 				} 
@@ -298,9 +384,24 @@ void checkEventPageQLV_DatVe(Shape *shape, short **mapID){
 			}
 	}
 }
-void checkEventPageQLV_DSCB(Shape *shape, short **mapID){
+void checkEventPageQLV_DSCB(Shape *shape, short **mapID,Input *input, Button *pre_next_Board, Board &board_DSCB_conve){
+	DanhSachMayBay dsMB;
+	layDSMayBay(dsMB);
+	DanhSachChuyenBay dsCB = NULL;
+	layDSChuyenBay(dsCB);
+	DanhSachTam dsDefault = dsNode2DsTmp(dsCB);
+	DanhSachTam dsTmp = dsDefault;
+	DanhSachTam dsFill;
+	
     int x = 0, y = 0;
-    short ID = -1, lastID = -1;
+    short ID = -1, lastID = -1, presentPage=1,chooseID = -1;
+    int numOfPage = (dsTmp.n - 1) / 10 + 1;
+    
+    updateNumOfBoardDSCB_conve(numOfPage, presentPage,dsTmp);
+    drawBoard(board_DSCB_conve,ID_DSCB_BOARD_3,mapID,shape);
+    dsFill = dsCBConVe(dsCB);
+	outDSCB(board_DSCB_conve, 1, 10,dsFill);
+	
     clearmouseclick(WM_LBUTTONDOWN);	
 	while(true){
 		delay(DELAY_TO_CHECK_EVENT);
@@ -316,6 +417,30 @@ void checkEventPageQLV_DSCB(Shape *shape, short **mapID){
 					case ID_ENTER_BUTTON_3 :{
 						return;
 					}
+					case ID_DSCB_PRE_BUTTON_3:{
+						if (presentPage > 1)
+                        {
+                           presentPage--;         			               
+                   	       updateNumOfBoardDSCB_conve(numOfPage, presentPage,dsTmp);
+                           drawBoard(board_DSCB_conve, (presentPage - 1) * 10 + ID_DSCB_BOARD_3, mapID, shape);
+                           outDSCB(board_DSCB_conve, 10 * (presentPage - 1) + 1, 10 * (presentPage),dsFill);
+            	        }
+						break;
+					}
+					case ID_DSCB_NEXT_BUTTON_3:{
+                        if (presentPage < numOfPage)
+                        {
+                           presentPage++;  	                			               
+                   	       updateNumOfBoardDSCB_conve(numOfPage, presentPage,dsTmp);					         					                                       
+                           drawBoard(board_DSCB_conve, (presentPage - 1) * 10 + ID_DSCB_BOARD_3, mapID, shape);
+                           outDSCB(board_DSCB_conve, 10 * (presentPage - 1) + 1, 10 * (presentPage),dsFill);
+				        }
+                        break;
+                    }
+                    	case ID_MA_CB_INPUT_3:{
+				getCBData(input,chooseID,dsCB,dsTmp);				                                 			
+				break;
+			}
 					default:
 						break;
 				}
@@ -381,16 +506,15 @@ void drawPageQLV_DSCB(short **mapID,Shape *shape){
 
 
 	Board board_DSCB_conve = {275,130,10,30,5,{125,125,180,350,120},{"MA CB","SO HIEU MB","NGAY GIO KHOI HANH","SAN BAY DEN","TRANG THAI"}};
-	drawBoard(board_DSCB_conve,ID_DSCB_BOARD_3,mapID,shape);///////////////////ID
 
 	setcolor(0);
     rectangle(275, 470, 1175, MAX_H);     
 
 
-	Input input= {625,569,15,250,0};
-	input.s = new char[input.max+2];	
-    drawInput(input, mapID,ID_MA_CB_INPUT_3);///////////////////ID
-    convertToShape(input, shape[ID_MA_CB_INPUT_3]);///////////////////ID
+	Input input[1]= {625,569,15,250,0};
+	input[0].s = new char[input[0].max+2];	
+    drawInput(input[0], mapID,ID_MA_CB_INPUT_3);///////////////////ID
+    convertToShape(input[0], shape[ID_MA_CB_INPUT_3]);///////////////////ID
 	setText();
 	outtextxy(500,575,"MA CHUYEN BAY:");
 
@@ -399,9 +523,9 @@ void drawPageQLV_DSCB(short **mapID,Shape *shape){
     drawButton(Enter_button[0], ID_ENTER_BUTTON_3 , mapID);
     convertToShape(Enter_button[0], shape[ID_ENTER_BUTTON_3]);
     
-    checkEventPageQLV_DSCB(shape,mapID);
+    checkEventPageQLV_DSCB(shape,mapID,input, pre_next_Board,board_DSCB_conve);
     
-    delete[] input.s;
+    delete[] input[0].s;
 }
 
 //===========================Page Ds Hanh Khach TrongCB==========================
